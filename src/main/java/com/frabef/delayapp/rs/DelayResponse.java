1bccaedbc53c37e4184ba7ce7e3fc1636383c7e6
Franco Iturrizaga <fiturrizagac@gmail.com>
2018-03-18 03:01:57 -0500

    Adding delay rest service

---
+++ src/main/java/com/frabef/delayapp/rs/DelayResponse.java
@@ -0,0 +1,13 @@
+package com.frabef.delayapp.rs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DelayResponse {
+
+    private String thread;
+}
